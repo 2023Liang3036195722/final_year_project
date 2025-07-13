c06064e754a23d7f97b91ad9c515da1e0400621e
Aaron020907 <u3638178@connect.hku.hk>
2025-07-13 19:53:27 +0800

    更新避免卡卡

--- StockMixer-master/new_src/train.py
+++ StockMixer-master/new_src/train.py
@@ -19,79 +19,82 @@ features_num = 5
 steps = 1
 scale_factor = 3
 
-# market_num = 10
-# learning_rate = 1e-3
-# alpha = 0.1
-
-
 # (83,735,5)
 data = np.load('stock_data_normalized.npy')
 close_price_data = data[:, :, -1]
 # 没有缺失数据，所有数据有效
 mask_data = np.ones((data.shape[0], data.shape[1]))
-
 # 计算一日收益率
 one_day_return_data = np.zeros((data.shape[0], data.shape[1]))
 for ticket in range(0, data.shape[0]):
     for row in range(1, data.shape[1]):
         one_day_return_data[ticket][row] = (data[ticket][row][-1] - data[ticket][row - steps][-1]) / \
                                data[ticket][row - steps][-1]
-
 trade_dates = mask_data.shape[1]
-model = StockMixer(
-    stocks=stock_num,
-    time_steps=lookback_length,
-    channels=features_num,
-    market=market_num,
-    scale=scale_factor
-).to(device)
-
-optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
-best_valid_loss = np.inf
-batch_offsets = np.arange(start=0, stop=valid_index, dtype=int)
 
 
-def validate(start_index, end_index):
-    with torch.no_grad():
-        cur_valid_pred = np.zeros([stock_num, end_index - start_index], dtype=float)
-        cur_valid_gt = np.zeros([stock_num, end_index - start_index], dtype=float)
-        cur_valid_mask = np.zeros([stock_num, end_index - start_index], dtype=float)
-        loss = 0.
-        reg_loss = 0.
-        rank_loss = 0.
-        for cur_offset in range(start_index - lookback_length - steps + 1, end_index - lookback_length - steps + 1):
-            data_batch, mask_batch, price_batch, gt_batch = map(
-                lambda x: torch.Tensor(x).to(device),
-                get_batch(cur_offset)
-            )
-            prediction = model(data_batch)
-            cur_loss, cur_reg_loss, cur_rank_loss, cur_rr = get_loss(prediction, gt_batch, price_batch, mask_batch,
-                                                                     stock_num, alpha)
-            loss += cur_loss.item()
-            reg_loss += cur_reg_loss.item()
-            rank_loss += cur_rank_loss.item()
-            cur_valid_pred[:, cur_offset - (start_index - lookback_length - steps + 1)] = cur_rr[:, 0].cpu()
-            cur_valid_gt[:, cur_offset - (start_index - lookback_length - steps + 1)] = gt_batch[:, 0].cpu()
-            cur_valid_mask[:, cur_offset - (start_index - lookback_length - steps + 1)] = mask_batch[:, 0].cpu()
-        loss = loss / (end_index - start_index)
-        reg_loss = reg_loss / (end_index - start_index)
-        rank_loss = rank_loss / (end_index - start_index)
-        cur_valid_perf = evaluate(cur_valid_pred, cur_valid_gt, cur_valid_mask)
-    return loss, reg_loss, rank_loss, cur_valid_perf
 
-def get_batch(offset=None):
-    if offset is None:
-        offset = random.randrange(0, valid_index)
-    seq_len = lookback_length
-    mask_batch = mask_data[:, offset: offset + seq_len + steps]
-    mask_batch = np.min(mask_batch, axis=1)
-    return (
-        data[:, offset:offset + seq_len, :],
-        np.expand_dims(mask_batch, axis=1),
-        np.expand_dims(close_price_data[:, offset + seq_len - 1], axis=1),
-        np.expand_dims(one_day_return_data[:, offset + seq_len + steps - 1], axis=1))
 
-def main():
+def main(config=None):
+    with wandb.init(config=config):
+        config = wandb.config
+        market_num = config.market_num
+        learning_rate = config.learning_rate
+        alpha = config.alpha
+
+    def validate(start_index, end_index):
+        with torch.no_grad():
+            cur_valid_pred = np.zeros([stock_num, end_index - start_index], dtype=float)
+            cur_valid_gt = np.zeros([stock_num, end_index - start_index], dtype=float)
+            cur_valid_mask = np.zeros([stock_num, end_index - start_index], dtype=float)
+            loss = 0.
+            reg_loss = 0.
+            rank_loss = 0.
+            for cur_offset in range(start_index - lookback_length - steps + 1, end_index - lookback_length - steps + 1):
+                data_batch, mask_batch, price_batch, gt_batch = map(
+                    lambda x: torch.Tensor(x).to(device),
+                    get_batch(cur_offset)
+                )
+                prediction = model(data_batch)
+                cur_loss, cur_reg_loss, cur_rank_loss, cur_rr = get_loss(prediction, gt_batch, price_batch, mask_batch,
+                                                                         stock_num, alpha)
+                loss += cur_loss.item()
+                reg_loss += cur_reg_loss.item()
+                rank_loss += cur_rank_loss.item()
+                cur_valid_pred[:, cur_offset - (start_index - lookback_length - steps + 1)] = cur_rr[:, 0].cpu()
+                cur_valid_gt[:, cur_offset - (start_index - lookback_length - steps + 1)] = gt_batch[:, 0].cpu()
+                cur_valid_mask[:, cur_offset - (start_index - lookback_length - steps + 1)] = mask_batch[:, 0].cpu()
+            loss = loss / (end_index - start_index)
+            reg_loss = reg_loss / (end_index - start_index)
+            rank_loss = rank_loss / (end_index - start_index)
+            cur_valid_perf = evaluate(cur_valid_pred, cur_valid_gt, cur_valid_mask)
+        return loss, reg_loss, rank_loss, cur_valid_perf
+
+    def get_batch(offset=None):
+        if offset is None:
+            offset = random.randrange(0, valid_index)
+        seq_len = lookback_length
+        mask_batch = mask_data[:, offset: offset + seq_len + steps]
+        mask_batch = np.min(mask_batch, axis=1)
+        return (
+            data[:, offset:offset + seq_len, :],
+            np.expand_dims(mask_batch, axis=1),
+            np.expand_dims(close_price_data[:, offset + seq_len - 1], axis=1),
+            np.expand_dims(one_day_return_data[:, offset + seq_len + steps - 1], axis=1))
+
+
+    model = StockMixer(
+        stocks=stock_num,
+        time_steps=lookback_length,
+        channels=features_num,
+        market=market_num,
+        scale=scale_factor
+    ).to(device)
+
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+    best_valid_loss = np.inf
+    batch_offsets = np.arange(start=0, stop=valid_index, dtype=int)
+
     for epoch in range(epochs):
         print("epoch{}------".format(epoch + 1))
         np.random.shuffle(batch_offsets)
